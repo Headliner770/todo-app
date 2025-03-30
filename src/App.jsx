64596ed19c7a3551b6ea55a8829b2de871642329
Headliner770 <paul248022@gmail.com>
2025-03-30 21:27:37 +0300

    Начало начал

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import './App.css'
+import { useState } from "react";
+import { ChangeTheme } from "./components/ChangeTheme.jsx";
+import "./App.css";
 
-function App() {
+// в ChangeTheme
+export default function App() {
+  const [theme, setTheme] = useState("light");
 
+  const changeTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
 
   return (
     <>
-   
+      <h1>To Do App</h1>
+      <button onClick={changeTheme}>Сhange Theme</button>
     </>
-  )
+  );
 }
-
-export default App
