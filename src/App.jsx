53bc595f1bad2556c8f123b8850a042142a1084e
Headliner770 <paul248022@gmail.com>
2025-04-02 21:20:34 +0300

    PR fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import Header from "./components/Header/Header.jsx";
-import AppContent from "./components/AppContent/AppContent.jsx";
+import { Header } from "./components/Header/Header.jsx";
+import { AppContent } from "./components/AppContent/AppContent.jsx";
+import { ThemeProvider } from "./components/Providers/ThemeProvider.jsx";
 import style from "./App.module.css";
 
 export default function App() {
   return (
-    <>
-      <Header />
-      <AppContent />
-    </>
+    <ThemeProvider>
+      <>
+        <Header />
+        <AppContent />
+      </>
+    </ThemeProvider>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,16 @@
+import { useContext } from "react";
 import ChangeTheme from "../ChangeTheme/ChangeTheme.jsx";
 import CurrentTime from "../CurrentTime.jsx";
+import { ThemeContext } from "../Providers/ThemeProvider.jsx";
 import styles from "./Header.module.css";
 
 export default function Header() {
+  const [theme, setTheme] = useContext(ThemeContext);
+
+  const adaptTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+  
   return (
     <header className={styles.header}>
       <h2>To Do App</h2>
