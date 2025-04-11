6b5a609ed82e7932bf60e27f59e696d442690783
Headliner770 <paul248022@gmail.com>
2025-04-11 22:40:17 +0300

    TasksPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import { Header } from "./components/Header/Header.jsx";
 import { ThemeProvider } from "./components/Providers/ThemeProvider.jsx";
 import { AccessManagement } from "./components/Auth/AccessManagement/AccessManagement.jsx";
 import { TasksPage } from "./components/TasksPage/TasksPage.jsx";
+import { AuthProvider } from "./components/Providers/AuthProvider.jsx";
 
 export default function App() {
   return (
     <ThemeProvider>
-      <Header />
-      <AccessManagement />
-      <TasksPage />
+     
+        <Header />
+        <AccessManagement />
+        <TasksPage />
+      
     </ThemeProvider>
   );
 }
--- src/components/Providers/AuthProvider.jsx
+++ src/components/Providers/AuthProvider.jsx
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export const AuthProvider = () => {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    const savedUser = localStorage.getItem("user");
+    if (savedUser) {
+      setUser(JSON.parse(savedUser));
+    }
+  }, []);
+
+  const login = (userData) => {
+    setUser(userData);
+    localStorage.setItem("user", JSON.stringify(userData));
+  };
+
+  const logout = () => {
+    setUser();
+    localStorage.removeItem("user");
+  };
+
   return;
 };
