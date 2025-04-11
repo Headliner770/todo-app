dc86c1d35218ba41e47c7f3d7115d217a17f3521
Headliner770 <paul248022@gmail.com>
2025-04-11 19:17:38 +0300

    taskPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Header } from "./components/Header/Header.jsx";
 import { ThemeProvider } from "./components/Providers/ThemeProvider.jsx";
 import { AccessManagement } from "./components/Auth/AccessManagement/AccessManagement.jsx";
 
+
 export default function App() {
   return (
     <ThemeProvider>
       <Header />
       <AccessManagement />
+      
     </ThemeProvider>
   );
 }
