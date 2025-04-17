27d458d7931d0db3cd60cb3f4669baec408937ee
Headliner770 <paul248022@gmail.com>
2025-04-17 14:12:09 +0300

    CategoriesPage

--- src/components/TasksPage/CategoriesPage/CategoriesPage.jsx
+++ src/components/TasksPage/CategoriesPage/CategoriesPage.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 import styles from "./CategoriesPage.module.css";
+import { useCategories } from "../../../hooks/useCategories";
 
 export const CategoriesPage = () => {
+  const { cateories, addCaategory, editCategory, deleteCategory } =
+    useCategories();
   const [newCategory, setNewCategory] = useState();
   const [editingCategory, setEditingCategory] = useState();
   const [saveEdit, setSaveEdit] = useState();
+
+const 
 
   return (
     <>
