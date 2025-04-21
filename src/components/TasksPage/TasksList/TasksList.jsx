f774c2f55c881cbe7b1135918d2deb6b1f2816a8
Headliner770 <paul248022@gmail.com>
2025-04-21 14:10:30 +0300

    TasksList

--- src/components/TasksPage/TasksList/TasksList.jsx
+++ src/components/TasksPage/TasksList/TasksList.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { useTasks } from "../../../hooks/useTasks.jsx";
+import { useCategories } from "../../../hooks/useCategories.jsx";
+import { Modal } from "../../Modal/Modal";
+import { PenLine, Trash2 } from "lucide-react";
+import { toast } from "react-toastify";
 
-const TasksList = () => {
-  return <div>TasksList</div>;
+export const TasksList = () => {
+  const { tasks, addTask, editTask, deleteTask, toggleComplete, receiveTasks } =
+    useTasks();
+  const { categories } = useCategories();
+
+  const [modalOpen, setModalOpen] = useState();
+  const [editTask, setEditTask] = useState();
+  const [taskTitle, setTaskTitle] = useState();
+
+  const OpenAddModal = () => {};
+
+  const OpenEditModal = () => {};
+
+  const CloseModal = () => {};
+
+  return (
+    <div>
+      <div>
+        <h2>Все задачи</h2>
+        <button>Добавить</button>
+      </div>
+      <div>
+        <div></div>
+      </div>
+
+      <Modal>
+        <form onSubmit={handle}></form>
+      </Modal>
+    </div>
+  );
 };
-
-export default TasksList;
